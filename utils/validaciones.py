0e5e38cee63dfb329b9455dcccbeefc839f2e1f6
Adrián Fúnez <adriianh@proton.me>
2026-07-25 15:16:30 -0600

    Implementa estructura inicial de utilidades y validaciones

---
+++ utils/validaciones.py
@@ -0,0 +1,18 @@
+def validar_cui(cui):
+    pass
+
+
+def validar_edad(edad):
+    pass
+
+
+def validar_sexo(sexo):
+    pass
+
+
+def validar_codigo(codigo):
+    pass
+
+
+def validar_grado(grado):
+    pass
