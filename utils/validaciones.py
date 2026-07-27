6c7a5992df98a008ae2c28b9019b4a01bd2fcff1
Adrián Fúnez <adriianh@proton.me>
2026-07-27 15:27:42 -0600

    Implementa validaciones de input

--- utils/validaciones.py
+++ utils/validaciones.py
@@ -1,18 +1,22 @@
 def validar_cui(cui):
-    pass
+    if not isinstance(cui, str):
+        return False
+    return len(cui) == 13 and cui.isdigit()
 
 
 def validar_edad(edad):
-    pass
+    if not isinstance(edad, int):
+        return False
+    return 3 <= edad <= 100
 
 
 def validar_sexo(sexo):
-    pass
+    return isinstance(sexo, str) and sexo.upper() in ["M", "F"]
 
 
 def validar_codigo(codigo):
-    pass
+    return isinstance(codigo, str) and len(codigo) > 0
 
 
 def validar_grado(grado):
-    pass
+    return isinstance(grado, int) and 1 <= grado <= 6
